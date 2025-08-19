6351b5ac083e0e6d9d83dfc5adcedd8a5edc5c0a
YORY31 <20230272@itla.edu.do>
2025-08-19 16:02:35 -0400

    update file yaml

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
